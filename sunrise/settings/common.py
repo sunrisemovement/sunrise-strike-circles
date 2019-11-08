dcc6d428b0b0644e6c46dc620e2dc508d79ac6dc
Jesse Evers <jesse27999@gmail.com>
2019-11-08 14:04:15 -0800

    Reconfigured static files

--- sunrise/settings/common.py
+++ sunrise/settings/common.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_statics/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
