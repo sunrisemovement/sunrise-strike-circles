3a4daa2803e6f6ff14c70e695c687822bd3993eb
Jesse Evers <jesse27999@gmail.com>
2019-11-08 23:19:27 -0800

    Make pledge_list responsive

--- pledgetovote/static/pledgetovote/js/pledge_list.js
+++ pledgetovote/static/pledgetovote/js/pledge_list.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   // Turn entire row into a link to the update form
   $('#pledge-list .columns').click(function() {
-    console.log('test');
     const editLink = $(this).data('link');
     window.location.replace(editLink);
   });
