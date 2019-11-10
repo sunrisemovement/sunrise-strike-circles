ad735326521b852cc787c3f2dea80edd8f542750
Jesse Evers <jesse27999@gmail.com>
2019-11-09 21:44:14 -0800

    Fixed image on UpdatePledge

--- pledgetovote/urls.py
+++ pledgetovote/urls.py
@@ -7,9 +7,8 @@ from pledgetovote.views import CreatePledge, Auth, PledgeList, SetLocation, Upda
 
 app_name = 'pledgetovote'
 urlpatterns = [
-    path('', lambda r: redirect('pledgetovote:pledge_list'), name='pledge_root'),
+    path('', PledgeList.as_view(), name='pledge_root'),
     path('auth/', Auth.as_view(), name='auth'),
-    path('pledges/', PledgeList.as_view(), name='pledge_list'),
     path('pledge/new/', CreatePledge.as_view(), name='pledge_new'),
     path('pledge/<int:pk>/', UpdatePledge.as_view(), name='pledge_edit'),
     path('set-location/', SetLocation.as_view(), name='set_location')
