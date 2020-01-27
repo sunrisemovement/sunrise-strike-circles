842bb2b7e39a340dea852d0a955b3bed64c162dd
Jesse Evers <jesse@jesseevers.com>
2020-01-27 03:49:43 -0500

    Updated graph

--- strikecircle/static/strikecircle/js/graph.js
+++ strikecircle/static/strikecircle/js/graph.js
@@ -6,8 +6,11 @@ const drawGraphs = () => {
     $('.graph').each(function(_, el) {
         const ctx = $(el);
 
+        const goal = parseInt($(el).data('goal'));
+        const goalType = $(el).data('goalType');
         const startWeek = parseInt($(el).data('startWeekNum'));
         const numWeeks = parseInt($(el).data('numWeeks'));
+
         const weekLabels = [];
         for (let i = startWeek; i <= startWeek + numWeeks; i++) {
             weekLabels.push(`Week ${i}`);
@@ -29,23 +32,39 @@ const drawGraphs = () => {
             type: 'line',
             data: {
                 datasets: [{
-                    label: $(el).data('label'),
                     fill: false,
                     data: parsedData,
-                    borderColor: '#222'
+                    borderColor: '#222',
+                    lineTension: 0  // Draw straight lines between points
                 }]
             },
             options: {
+                maintainAspectRatio: false,
+                legend: { display: false },
+                tooltips: {
+                    callbacks: {
+                        title: tooltipItem => `${tooltipItem[0].value} ${goalType}`,
+                        label: _ => false
+                    }
+                },
                 scales: {
                     xAxes: [{
                         type: 'category',
                         labels: weekLabels
                     }],
                     yAxes: [{
-                        type: 'linear',
+                        gridLines: {
+                            borderDash: [5, 15],
+                            lineWidth: 2
+                        },
                         ticks: {
-                            beginAtZero: true,
-                            precision: 0
+                            min: 0,
+                            max: Math.trunc(goal * 1.15),
+                            callback: value => `Goal: ${value} ${goalType}`
+                        },
+                        afterBuildTicks: axis => {
+                            axis.ticks = [];
+                            axis.ticks.push(goal);
                         }
                     }]
                 }
