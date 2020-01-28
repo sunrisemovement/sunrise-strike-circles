c0bac7468d79820ad409945486f15537c36833c6
Jesse Evers <jesse@jesseevers.com>
2020-01-28 01:41:28 -0500

    New data input page/form (#18)
    * Change listdisplay to leaderboard, move table CSS to table.sass, rename templatetags
    
    * Deleted unused signup form
    
    * Add data input dashboard, reconfigure URLconf to use it, minor Sass changes

--- strikecircle/urls.py
+++ strikecircle/urls.py
@@ -3,12 +3,12 @@ from django.shortcuts import redirect
 from django.urls import path
 from django.conf.urls.static import static
 
-from strikecircle.views import DataEntry, ProgramGuide, ProgressDashboard, UpdateStrikeCircle
+from strikecircle.views import DataInput, ProgramGuide, ProgressDashboard, UpdateStrikeCircle
 
 app_name = 'strikecircle'
 urlpatterns = [
     path('', ProgressDashboard.as_view(), name='dashboard'),
-    path('data-entry/', DataEntry.as_view(), name='data_entry_dash'),
+    path('data-entry/', DataInput.as_view(), name='data_input_dash'),
     path('profile/', UpdateStrikeCircle.as_view(), name='sc_edit'),
     path('program-guide/', ProgramGuide.as_view(), name='program_guide'),
 ]
