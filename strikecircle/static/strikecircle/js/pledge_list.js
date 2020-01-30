b82abbf1e2d0c78a1afcdd03e37ee3d974e9df1e
Jesse Evers <jesse@jesseevers.com>
2020-01-30 02:09:30 -0500

    Deleted unneeded files from deprecated Pledge to Vote site, and old input form

--- strikecircle/static/strikecircle/js/pledge_list.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  // Turn entire row into a link to the update form, unless there aren't any pledges
-  if (!$('#pledge-list .columns#no-pledges-container').length) {
-    $('#pledge-list .columns').click(function() {
-      const editLink = $(this).data('link');
-      window.location.assign(editLink);
-    });
-  }
-});
